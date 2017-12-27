3da1c30c077b0d0e64ed1279381291d173c2db85
iNekox3 <inekox3@hotmail.com>
2017-12-27 17:05:45 +0800

    Essential Animations and Prefabs Created With Sprites
    All current essential sprites for game screen were loaded in.
    
    Animations and controllers have been created as well as prefabs for essentially game objects:
    - Background
    - Player controls
    - UI buttons
    - Projectiles
    - Next panel and color
    
    New font asset BRUSHSTP created with TextMeshPro. BRUSHSTP font was taken from fontsquirrel under assumed license of free use (i.e. license not found).
    
    To be fixed:
    - Proportion of graphics to the screen as issues were seen on mobile build during preview

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public Animator animator;
+    private bool hasSpeed; // determines outline color of projectile
+                           // false = brown; true = yellow
+
+    private Transform fill; // child of this gameObject
+    private string[] fillColors = { "Red", "Blue", "Yellow", "Green", "Orange", "Purple", "Grey" };
+
+    // Use this for initialization
+    void Start () {
+        // Initialize child
+        fill = this.gameObject.transform.GetChild(0);
+        hasSpeed = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    // Toggle color of outline between brown (normal) and yellow (sped up)
+    public void toggleOutlineColor() {
+        animator.SetBool("hasSpeed", hasSpeed);
+    }
+
+    // Change the color of the projectile given the color index
+    public void changeFillColor(int colorIndex) {
+        // Get child's animator component and change to the specified color animation
+        fill.GetComponent<Animator>().Play("Projectile" + fillColors[colorIndex]);
+    }
+}
